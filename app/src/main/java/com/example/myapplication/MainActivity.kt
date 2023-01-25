8534e00fe0adb4ae067bbcb4357d92b906230b89
pochapali <ivantrufan27@gmail.com>
2023-01-25 23:56:00 +0200

    Now it does BMI

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,41 +4,74 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HeartRateApp()
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color=MaterialTheme.colors.background
+
+                ) {
+                    Bmi()
+                }
+
         }
     }
 }
 
 @Composable
-fun HeartRateApp() {
-    var ageInput by remember { mutableStateOf(value = "") }
-    val age = ageInput.toIntOrNull() ?: 044
-    val upper = if (age>0) (220-age) * 0.85 else 0
-    val lower = if (age>0) (220-age) * 0.65 else 0
+fun Bmi() {
+    var heightInput: String by remember {
+        mutableStateOf(value= "")
+    }
+    var weightInput : String by remember {
+        mutableStateOf(value = "")
+    }
+    val height = heightInput.toFloatOrNull()?:0.0f
+    val weight = weightInput.toIntOrNull()?:0
+    val bmi = if (weight>0 && height>0) weight/ (height*height) else 0.0
+
 
     Column() {
         Text(
-            text="HRL",
-            style= MaterialTheme.typography.h6
+            text="Body mass index ",
+            fontSize = 24.sp,
+            color = MaterialTheme.colors.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp)
         )
-        TextField(
-            value = ageInput,
-            onValueChange = {ageInput=it},
-            label={Text(text="Age")}
+        OutlinedTextField(
+            value = heightInput,
+            onValueChange = {heightInput=it},
+            label={Text(text="Height")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType=KeyboardType.Number   )
         )
-        Text(
-            text = "Limits are $lower - $upper"
+        OutlinedTextField(
+            value = weightInput,
+            onValueChange = {weightInput=it},
+            label={Text(text="Weight")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType=KeyboardType.Number   )
         )
-
+        Text(text = "Body mass index is "+bmi)
     }
 }
 
